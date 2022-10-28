24be2291224061a01a75ef428c0206c91dacb403
K_null <talagod0@gmail.com>
2022-10-29 01:13:50 +0700

    commit map

--- Assets/Scripts/Platform/PlatformMovement.cs
+++ Assets/Scripts/Platform/PlatformMovement.cs
@@ -37,13 +37,29 @@ public class PlatformMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        /*if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }*/
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+       /* if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.SetParent(transform);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
